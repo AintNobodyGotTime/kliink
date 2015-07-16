c23cc11a9c0837ed6f8be5be80e89773497462bc
Emily Hittle <emilyliza@gmail.com>
2015-07-16 14:58:21 -0700

    cleaned up route.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+	
+	devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
+	resources :users
+  
   get 'site/index'
   
   root to: "site#index"
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
-	resources :users
+  
 end
