5874fc1ae0a906ddea72f930f64ea4b4d8a9503c
Jon1213 <jon1213@gmail.com>
2015-07-16 14:27:40 -0700

    mild migration changes

--- db/migrate/20150716122358_devise_create_users.rb
+++ db/migrate/20150716122358_devise_create_users.rb
@@ -1,6 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
-    create_table(:users) do |t|
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -30,7 +30,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      
+
       t.timestamps
     end
 
