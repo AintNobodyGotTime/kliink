feed0ee5a527df4526d54d1da4e9d725bb97e64d
Emily Hittle <emilyliza@gmail.com>
2015-07-16 18:11:56 -0700

    changed name of project_item_comment controller

--- app/controllers/project_item_comment_controller.rb
+++ app/controllers/project_item_comment_controller.rb
@@ -1,74 +1,81 @@
 class ProjectItemCommentController < ApplicationController
-  before_action :set_controller, only: [:show, :edit, :update, :destroy]
+  before_action :set_project_item_comment, only: [:show, :edit, :update, :destroy]
 
-  # GET /controllers
-  # GET /controllers.json
+  # GET /project_item_comments
+  # GET /project_item_comments.json
   def index
-    @controllers = Controller.all
+    @project_item_comments = ProjectItemComment.all
   end
 
-  # GET /controllers/1
-  # GET /controllers/1.json
+  # GET /project_item_comments/1
+  # GET /project_item_comments/1.json
   def show
   end
 
-  # GET /controllers/new
+  # GET /project_item_comments/new
   def new
-    @controller = Controller.new
+    @project_item_comment = ProjectItemComment.new
   end
 
-  # GET /controllers/1/edit
+  # GET /project_item_comments/1/edit
   def edit
   end
 
-  # POST /controllers
-  # POST /controllers.json
+  # POST /project_item_comments
+  # POST /project_item_comments.json
   def create
-    @controller = Controller.new(controller_params)
+    @project_item_comment = ProjectItemComment.new(project_item_comment_params)
+
+    if @project_item_comment.save
+      user_signed_in?
+        user = current_user
+        user.total_points += 3 
+        @project_item_comment.point_source = "comment"
+      end
 
     respond_to do |format|
-      if @controller.save
-        format.html { redirect_to @controller, notice: 'Controller was successfully created.' }
-        format.json { render :show, status: :created, location: @controller }
+      if @project_item_comment.save
+        format.html { redirect_to @project_item_comment, notice: 'Project item comment was successfully created.' }
+        format.json { render :show, status: :created, location: @project_item_comment }
       else
         format.html { render :new }
-        format.json { render json: @controller.errors, status: :unprocessable_entity }
+        format.json { render json: @project_item_comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /controllers/1
-  # PATCH/PUT /controllers/1.json
+  # PATCH/PUT /project_item_comments/1
+  # PATCH/PUT /project_item_comments/1.json
   def update
     respond_to do |format|
-      if @controller.update(controller_params)
-        format.html { redirect_to @controller, notice: 'Controller was successfully updated.' }
-        format.json { render :show, status: :ok, location: @controller }
+      if @project_item_comment.update(project_item_comment_params)
+        format.html { redirect_to @project_item_comment, notice: 'Project item comment was successfully updated.' }
+        format.json { render :show, status: :ok, location: @project_item_comment }
       else
         format.html { render :edit }
-        format.json { render json: @controller.errors, status: :unprocessable_entity }
+        format.json { render json: @project_item_comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /controllers/1
-  # DELETE /controllers/1.json
+  # DELETE /project_item_comments/1
+  # DELETE /project_item_comments/1.json
   def destroy
-    @controller.destroy
+    @project_item_comment.destroy
     respond_to do |format|
-      format.html { redirect_to controllers_url, notice: 'Controller was successfully destroyed.' }
+      format.html { redirect_to project_item_comments_url, notice: 'Project item comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_controller
-      @controller = Controller.find(params[:id])
+    def set_project_item_comment
+      @project_item_comment = ProjectItemComment.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def controller_params
-      params.require(:controller).permit(:item_comment)
+    def project_item_comment_params
+      params[:project_item_comment]
     end
 end
