16a8165ac403c3694dbfe9099f6189f781a39fdc
Emily Hittle <emilyliza@gmail.com>
2015-07-23 17:23:20 -0700

    swapped two images on show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,10 @@ Project.create(description:
 ', name: 'Sukrupa’s Rajendra School Project', ngo_id:1)
 
 
-ProjectItem.create(title: 'Clearing the land', content: 'The school is built on almost 2 acres.', project_id:1)
-ProjectItem.create(title: 'Laying the bricks', content: 'It takes a crew of 40+ workers to construct the school. These workers have been working all day for weeks on end to get this structure up. Many of the worker\'s children will attend the school.', project_id:1)
-ProjectItem.create(title:'Installing furniture', content: 'Desks and chairs were made locally at a furniture factory in north Bangalore. The lumber used to make the seats and chairs are sourced within 400 km and are taken from sustainable foresting.', project_id:1 )
-ProjectItem.create(title: 'Library books', content: 'Over 700 books will fill the library', project_id:1)
-ProjectItem.create(title: 'Computer Lab', content: '10 Computers will be used by the children. Each class gets computer time once a week to learn valuable 21 century', project_id:1)
-ProjectItem.create(title:'School bus arrives', content:'The school bus will be used for children who live more than 4km away from the school', project_id:1)
+ProjectItem.delete_all
+# ProjectItem.create(title: 'Clearing the land', content: 'The school is built on almost 2 acres.', project_id:1)
+# ProjectItem.create(title: 'Laying the bricks', content: 'It takes a crew of 40+ workers to construct the school. These workers have been working all day for weeks on end to get this structure up. Many of the worker\'s children will attend the school.', project_id:1)
+# ProjectItem.create(title:'Installing furniture', content: 'Desks and chairs were made locally at a furniture factory in north Bangalore. The lumber used to make the seats and chairs are sourced within 400 km and are taken from sustainable foresting.', project_id:1 )
+# ProjectItem.create(title: 'Library books', content: 'Over 700 books will fill the library', project_id:1)
+# ProjectItem.create(title: 'Computer Lab', content: '10 Computers will be used by the children. Each class gets computer time once a week to learn valuable 21 century', project_id:1)
+# ProjectItem.create(title:'School bus arrives', content:'The school bus will be used for children who live more than 4km away from the school', project_id:1)
